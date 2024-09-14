31e65fd0aa0557fade16136088938e0e3d247efe
Ateja <atejukas123@gmail.com>
2024-09-14 15:39:34 +0100

    each topic metadata added

--- news-article-topics/src/app/topic/[id]/page.js
+++ news-article-topics/src/app/topic/[id]/page.js
@@ -1,3 +1,29 @@
+export async function generateMetadata({ params }) {
+  const { id } = params;
+  const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+  );
+  const data = await res.json();
+  const article = data.articles[id];
+
+  if (!article) {
+    return {
+      title: "Article Not Found",
+      description: "The article you are looking for does not exist.",
+    };
+  }
+
+  return {
+    title: `${article.title} - Online Newspaper`,
+    description:
+      article.description ||
+      `Read more about the latest news on ${article.title}`,
+    keywords: `${article.title}, news, latest news, online newspaper`,
+  };
+}
+
 export default async function Topic({ params }) {
   const { id } = params;
 
